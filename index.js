1c3a0c171ba5f70df57b0520354ca4a1e6741cc1
ctscofield <ctscofield@gmail.com>
2021-07-08 15:30:20 -0600

    setting up react

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const dotenv = require("dotenv").config();
+const dotenv = require("dotenv").config(); //eslint-disable-line
+
 const express = require("express");
 const cors = require("cors");
 const server = express();
 const PORT = process.env.PORT || 5000;
+const path = require("path");
 
 server.use(cors());
 server.use(express.json());
+server.use(express.static(path.join(__dirname, "client/build")));
 
-server.get("/api", (req, res) => {
-    res.json({ message: `What the heck is even going on right now!?`})
+
+server.use("/api", (req, res) => {
+    res.json({ message: `What the heck is even going on right now on ${PORT}!?`})
 })
 
 server.listen(PORT, () => {
