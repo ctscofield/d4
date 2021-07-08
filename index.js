c4a2c38d0f34d2e9671f59cd83cb8a0edd25b28e
ctscofield <ctscofield@gmail.com>
2021-07-08 13:16:58 -0600

    created react app

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-require("dotenv").config();
+const dotenv = require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const server = express();
-
 const PORT = process.env.PORT || 5000;
+
+server.use(cors());
+server.use(express.json());
 
 server.get("/api", (req, res) => {
     res.json({ message: `What the heck is even going on right now!?`})
